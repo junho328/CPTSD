457680a9a5bd00f5b75a579cdefa502b20758a3f
anne-chloe <anne-chloe.joblin@charite.de>
2025-01-14 13:47:02 +0100

    Update utils.py
    Translation

--- extraction/utils.py
+++ extraction/utils.py
@@ -15,12 +15,12 @@ def calculate_metrics(filename):
     accuracy_plus = (data_filtered['Correct2'] == 1).sum() / len(data_filtered)
 
     # Calculating Sensitivity
-    sensitivity_rows = data_filtered[(data_filtered['Symptom'] != '해당없음') & (data_filtered['Estimated symptom'] != '해당없음')]
-    sensitivity = len(sensitivity_rows) / len(data_filtered[data_filtered['Symptom'] != '해당없음'])
+    sensitivity_rows = data_filtered[(data_filtered['Symptom'] != 'Not applicable') & (data_filtered['Estimated symptom'] != ''Not applicable')]
+    sensitivity = len(sensitivity_rows) / len(data_filtered[data_filtered['Symptom'] != 'Not applicable'])
 
     # Calculating Specificity
-    specificity_rows = data_filtered[(data_filtered['Symptom'] == '해당없음') & (data_filtered['Estimated symptom'] == '해당없음')]
-    specificity = len(specificity_rows) / len(data_filtered[data_filtered['Symptom'] == '해당없음'])
+    specificity_rows = data_filtered[(data_filtered['Symptom'] == 'Not applicable') & (data_filtered['Estimated symptom'] == 'Not applicable')]
+    specificity = len(specificity_rows) / len(data_filtered[data_filtered['Symptom'] == 'Not applicable'])
 
     result = {}
     result['Accuracy'] = accuracy
