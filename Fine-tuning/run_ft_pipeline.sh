9382814b439f08e94ef9b87b1e89043c2963afc8
realsotrue <104465971+realsotrue@users.noreply.github.com>
2025-04-11 02:06:17 +0900

    Update run_ft_pipeline.sh

--- Fine-tuning/run_ft_pipeline.sh
+++ Fine-tuning/run_ft_pipeline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+python3 symp_gpt3.5_ft.py --p P1
+python3 symp_gpt3.5_ft.py --p P2
 python3 symp_gpt3.5_ft.py --p P3
-python3 symp_gpt3.5_ft.py --p P7
-python3 symp_gpt3.5_ft.py --p P9
-python3 symp_gpt3.5_ft.py --p P13
+python3 symp_gpt3.5_ft.py --p P4
 
 python3 get_metric.py --model_name ur_model
